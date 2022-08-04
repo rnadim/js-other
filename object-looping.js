8985b1c5e26f913bc9c8a67094e8219fca633547
rnadim <rmohibur010@mail.com>
2022-08-04 22:44:50 +0600

    new add

--- object-looping.js
+++ object-looping.js
@@ -1,12 +1,4 @@
-var shoppingCart = {
-    books: 3,
-    sunglass: 1,
-    keyboard: 5,
-    mouse: 1,
-    pen: 25,
-    shoes: 2
-}
-
+// array vs object 
 var shoppingItems = ['books', 'sunglass', 'shoes', 'pen'];
 var friendsAge = [12, 45, 78, 12, 32, 14];
 var friendsAge = {
@@ -17,4 +9,30 @@ var friendsAge = {
     robi: 36,
     tarek: 65
 
+}
+var shoppingCart = {
+    books: 3,
+    sunglass: 1,
+    keyboard: 5,
+    mouse: 1,
+    pen: 25,
+    shoes: 2
+}
+
+const keys = Object.keys(shoppingCart);
+console.log(keys);
+
+const values = Object.values(shoppingCart);
+console.log(values)
+
+for (var i = 0; i < keys.length; i++) {
+    var propertyName = keys[i];
+    var propertyValues = shoppingCart[propertyName];
+    console.log(propertyName, propertyValues);
+}
+
+// for in loop
+for (var propertyName in shoppingCart) {
+    const value = shoppingCart[propertyName];
+    console.log(propertyName, value);
 }
---
+++ switch.js
@@ -0,0 +1,42 @@
+const color = 'yellow';
+if (color === 'green') {
+    console.log('you are my best friend');
+}
+else if (color === 'blue') {
+    console.log('you are your best friend');
+}
+else if (color === 'red') {
+    console.log('you are my friend');
+}
+else if (color === 'white') {
+    console.log('you are best friend');
+}
+else if (color === 'yellow') {
+    console.log('you are my notyest friend');
+}
+else {
+    console.log('you are my black friend');
+}
+
+// switch
+switch (color) {
+    case ' green':
+        console.log('you are my best friend');
+        break;
+    case 'blue':
+        console.log('you are my best friend');
+        break;
+    case 'white':
+        console.log('you are my best friend');
+        break;
+    case 'yellow':
+        console.log('you are my best friend');
+        break;
+    case 'red':
+        console.log('you are my best friend');
+        break;
+    default:
+        console.log('you are a green friend');
+
+
+}
